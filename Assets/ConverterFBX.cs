9395745f14a6bf76b2039156611c62f82d1434df
Mixail <mixail.korchikov@mail.ru>
2020-07-20 14:35:03 +0300

    init

---
+++ Assets/ConverterFBX.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine; 
+using UnityFBXExporter;
+
+public class ConverterFBX : MonoBehaviour
+{
+    [Header("For Spatial Awareness System")]
+    [SerializeField] private GameObject objMeshToExport;   
+    [SerializeField] private bool canGenerate = false;
+
+    void Start()
+    {
+        if (canGenerate && objMeshToExport != null)
+        {
+            string path = Path.Combine(Application.persistentDataPath, "data");
+            path = Path.Combine(path, objMeshToExport.name + ".fbx");
+
+            if (!Directory.Exists(Path.GetDirectoryName(path)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+
+            FBXExporter.ExportGameObjToFBX(objMeshToExport, path, true, true);
+            Debug.Log($"Файл {objMeshToExport.name}.fbx создан, в {path}");
+        }
+    }
+}
